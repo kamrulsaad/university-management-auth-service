082ae2b01ac4f4589927fff6735a02edec5bd0f4
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-06-09 23:02:43 +0600

    Created Semester service with proper validation

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -24,8 +24,17 @@ export const AcademicSemesterTitles: IAcademicSemesterTitles[] = [
   'Summer',
   'Fall',
 ];
+
 export const AcademicSemesterCodes: IAcademicSemesterCodes[] = [
   '01',
   '02',
   '03',
 ];
+
+export const AcademicSemesterTitleCodeMapper: {
+  [key: string]: string;
+} = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,16 @@
+import ApiError from '../../../errors/ApiError';
+import { AcademicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
+import status from 'http-status';
 
 const createAcademicSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (AcademicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError('Invalid semester code', status.BAD_REQUEST);
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
