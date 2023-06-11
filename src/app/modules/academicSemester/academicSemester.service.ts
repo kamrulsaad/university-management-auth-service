13a9b538df67a64802a0b569106700590a980f86
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-06-11 23:17:34 +0600

    [DONE] Added pagination and sorting feature

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,3 +1,5 @@
+import status from 'http-status';
+import { SortOrder } from 'mongoose';
 import ApiError from '../../../errors/ApiError';
 import { paginationHelper } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
@@ -5,7 +7,6 @@ import IPaginationOptions from '../../../interfaces/pagination';
 import { AcademicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
-import status from 'http-status';
 
 const createAcademicSemester = async (
   payload: IAcademicSemester
@@ -21,10 +22,19 @@ const createAcademicSemester = async (
 const getAllSemesters = async (
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
-  const { page, limit, skip } =
+  const { page, limit, skip, sortBy, sortOrder } =
     paginationHelper.calculatePagination(paginationOptions);
 
-  const result = await AcademicSemester.find().sort().skip(skip).limit(limit);
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const result = await AcademicSemester.find()
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
 
   const total = await AcademicSemester.countDocuments();
 
--- src/helpers/paginationHelper.ts
+++ src/helpers/paginationHelper.ts
@@ -1,12 +1,18 @@
+import { SortOrder } from 'mongoose';
+
 type IOptions = {
   page?: number;
   limit?: number;
+  sortBy?: string;
+  sortOrder?: SortOrder;
 };
 
 type IOptionsResult = {
   page: number;
   limit: number;
   skip: number;
+  sortBy: string;
+  sortOrder: SortOrder;
 };
 
 const calculatePagination = (options: IOptions): IOptionsResult => {
@@ -15,10 +21,15 @@ const calculatePagination = (options: IOptions): IOptionsResult => {
 
   const skip = (page - 1) * limit;
 
+  const sortBy = options.sortBy || 'createdAt';
+  const sortOrder = options.sortOrder || 'desc';
+
   return {
     page,
     limit,
     skip,
+    sortBy,
+    sortOrder,
   };
 };
 
