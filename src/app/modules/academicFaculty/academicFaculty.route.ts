cef19f1c4796d040850a1e67cedb0cdc942bc224
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-07-03 04:45:20 +0600

    Complete module 17

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -2,24 +2,45 @@ import express from 'express';
 import { AcademicFacultyController } from './academicFaculty.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyValidation } from './academicFaculty.validation';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post(
   '/create-faculty',
   validateRequest(AcademicFacultyValidation.createFacultyZodSchema),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   AcademicFacultyController.createAcademicFaculty
 );
 
-router.get('/', AcademicFacultyController.getAllFaculties);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    // ENUM_USER_ROLE.STUDENT,
+    ENUM_USER_ROLE.SUPER_ADMIN
+  ),
+  AcademicFacultyController.getAllFaculties
+);
 
 router.get('/:id', AcademicFacultyController.getSingleFaculty);
 
 router.patch(
   '/:id',
   validateRequest(AcademicFacultyValidation.updateFacultyZodSchema),
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.SUPER_ADMIN
+  ),
   AcademicFacultyController.updateFaculty
 );
 
-router.delete('/:id', AcademicFacultyController.deleteFaculty);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicFacultyController.deleteFaculty
+);
 
 export const AcademicFacultyRoutes = router;
